8c0c965b0c048c23580c578cdb3332265861bf23
langletamelie <amelie.langlet@hotmail.com>
2018-06-25 10:06:39 +0200

    exo6

---
+++ assets/js/js6.js
@@ -0,0 +1,32 @@
+//déclarartion des fonctions
+function changeImage(){
+document.getElementById('image1').src = 'assets/images/image1_2.jpg';
+}
+function changeImage2(){
+document.getElementById('image2').src = 'assets/images/image2_2.jpg';
+}
+function changeImage3(){
+document.getElementById('image3').src = 'assets/images/image3_2.jpg';
+  }
+function changeImage4(){
+document.getElementById('image4').src = 'assets/images/image4_2.jpg';
+}
+function changeImage5(){
+document.getElementById('image5').src = 'assets/images/image5_2.jpg';
+}
+//déclaration de la deuxième fonction
+function change(){
+document.getElementById('image1').src = 'assets/images/image1.jpg';
+}
+function change2(){
+document.getElementById('image2').src = 'assets/images/image2.jpg';
+}
+function change3(){
+document.getElementById('image3').src = 'assets/images/image3.jpg';
+}
+function change4(){
+document.getElementById('image4').src = 'assets/images/image4.jpg';
+}
+function change5(){
+document.getElementById('image5').src = 'assets/images/image5.jpg';
+}
